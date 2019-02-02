b0d1a2f950ff70fe28a36413a919065820c93220
bukefal <buckefalus@gmail.com>
2019-02-02 12:34:52 +0100

    Ovo je ipak klasa Student

---
+++ src/Student/Student.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Student;
+
+/**
+ *
+ * @author qa-popodne
+ */
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private int yearOfBirth;
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, int yearOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public void info() {
+        System.out.println("First Name : " + this.firstName);
+        System.out.println("Last Name : " + this.lastName);
+        System.out.println("Year of Birth : " + this.yearOfBirth);
+    }
+}
